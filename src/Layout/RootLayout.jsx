551f232c3c05f7b344b0cf4af29e344bc3a6005b
Mizanur Rahman <44541965+HelloMizanur@users.noreply.github.com>
2026-04-16 16:28:08 +0600

    Implement dynamic document titles in RootLayout
    Add dynamic document title management based on route.

--- src/Layout/RootLayout.jsx
+++ src/Layout/RootLayout.jsx
@@ -1,13 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Navbar from "./../Components/shared/Navbar";
 import Footer from "../Components/shared/Footer";
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 
 const RootLayout = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const titles = {
+      "/": "Home | KeenKeeper",
+      "/timeline": "Timeline | KeenKeeper",
+      "/stats": "Statistics | KeenKeeper",
+    };
+
+    if (location.pathname.startsWith("/friendDetail/")) {
+      document.title = "Friend Profile | KeenKeeper";
+    } else {
+      document.title = titles[location.pathname] || "KeenKeeper";
+    }
+  }, [location]);
+
   return (
     <>
       <Navbar />
-      <Outlet />
+      <div className="min-h-[calc(100vh-200px)]">
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
